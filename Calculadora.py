35a18dda997ce0f4819e39b76ac555eec7bf5cac
Bruno Mota <contatobrunotr@gmail.com>
2025-02-23 17:50:11 -0300

    :recycle: refactor: lógica escolha
    Movida a lógica de escolha para a classe a qual pertence

--- Calculadora.py
+++ Calculadora.py
@@ -2,21 +2,38 @@ class Calculadora:
     def __init__(self, numeroUm, numeroDois):
         self.numeroUm = numeroUm
         self.numeroDois = numeroDois
-        
 
     def calculoAdicao(self):
         return self.numeroUm + self.numeroDois
-    
+
     def calculoSubtracao(self):
         return self.numeroUm - self.numeroDois
-    
-    def caluloMultiplicacao(self):
+
+    def calculoMultiplicacao(self):
         return self.numeroUm * self.numeroDois
-    
+
     def calculoDivisao(self):
         try:
             return self.numeroUm / self.numeroDois
         except ZeroDivisionError:
-            return print("DIVISÃO POR ZERO NÃO É PERMITIDA, TENTE NOVAMENTE!")    
+            print("DIVISÃO POR ZERO NÃO É PERMITIDA, TENTE NOVAMENTE!")
+            return None
 
+    def calcular(self, operacao):
+        
+        if operacao == 1:
+            print("Resultado:", self.calculoAdicao())
 
+        elif operacao == 2:
+            print("Resultado:", self.calculoSubtracao())
+
+        elif operacao == 3:
+            print("Resultado:", self.calculoMultiplicacao())
+
+        elif operacao == 4:
+            resultado = self.calculoDivisao()
+            if resultado is not None:  
+                print("Resultado:", resultado)
+
+        else:
+            print("ESCOLHA INCORRETA, TENTE NOVAMENTE!")
--- app.py
+++ app.py
@@ -1,19 +1,10 @@
-from Calculadora import Calculadora
+from Calculadora import Calculadora  
 
-escolherOperacao = input("Digite (1) para Adição, (2) para Subtração, (3) para Multiplicação ou (4) para Divisão: ")
+numeroUm = int(input("Digite o primeiro número para o cálculo: "))
+numeroDois = int(input("Digite o segundo número para o cálculo: "))
 
-calcConta = Calculadora(int(input("Digite o primeiro numero para o calculo: ")), int(input("Digite o segundo numero para o calculo: ")))
+calcConta = Calculadora(numeroUm, numeroDois)
 
-if escolherOperacao == 1:
-    print("Resultado: ", calcConta.calculoAdicao())
+escolherOperacao = int(input("Digite (1) para Adição, (2) para Subtração, (3) para Multiplicação ou (4) para Divisão: "))
 
-elif escolherOperacao == 2:
-    print("Resultado: ", calcConta.calculoSubtracao())
-    
-elif escolherOperacao == 3:
-    print("Resultado: ", calcConta.caluloMultiplicacao())
-
-elif escolherOperacao == 4:
-    print("Resultado: ", calcConta.calculoDivisao())
-else:
-    print("ESCOLHA INCORRETA, TENTE NOVAMENTE!")
+calcConta.calcular(escolherOperacao)
